81450662126cf182e49a41c0bdc27554ff9cd42e
Sachin Kumar <sachinkum123567@gmail.com>
2026-08-04 22:38:39 +0200

    base file for astar planner added
    Signed-off-by: Sachin Kumar <sachinkum123567@gmail.com>

--- crates/mini-nav-planner/src/planner.rs
+++ crates/mini-nav-planner/src/planner.rs
@@ -1,2 +1,7 @@
-/// Planner
-pub struct Planner;
+pub mod a_star;
+
+/// Planner Trait
+pub trait Planner {
+    /// Plans the trajectory from start to goal position
+    fn plan(&self, start: &[f32; 2], goal: &[f32; 2]) -> Vec<f32>;
+}
---
+++ crates/mini-nav-planner/src/planner/a_star.rs
@@ -0,0 +1,10 @@
+// TODO: Implement A Star Algorithm for path planning
+
+/// # A Star
+///
+/// Path planning algorithm with heuristics
+pub struct AStar {
+
+}
+
+
